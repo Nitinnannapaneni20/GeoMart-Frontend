b6dbfc4d97840abd72886d4d87654f1455c74ee5
nitin <nitinnannapaneni@gmail.com>
2024-11-02 02:23:19 +0000

    Working log in log out

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,3 +1,17 @@
+
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleAuth({
+        authorizationParams: {
+          audience: process.env.AUTH0_AUDIENCE,
+          scope: 'openid profile email',
+        }
+      })(req, res);
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
