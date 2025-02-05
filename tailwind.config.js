4ddfb6cc16fe6941a329ca7aff23e1d105128186
nitin <nitinnannapaneni@gmail.com>
2025-02-05 18:36:25 +0000

    home page moved to tailwind and dark light mode now working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // Ensure "class" mode is enabled
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
