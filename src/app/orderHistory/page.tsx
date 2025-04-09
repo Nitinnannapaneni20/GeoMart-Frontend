b17967718e4c5c53b9fcd73f2659ebfd7020df3b
nitin <nitinnannapaneni@gmail.com>
2025-04-09 17:54:04 +0100

    Order History default page added

---
+++ src/app/orderHistory/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Header from "@/components/Header";
+
+export default function OrderHistory() {
+
+  return (
+   <>
+         <Header />
+         <main className="bg-white min-h-screen flex items-center justify-center">
+           <h2 className="text-2xl">Order History Content Goes Here</h2>
+         </main>
+       </>
+  );
+}
