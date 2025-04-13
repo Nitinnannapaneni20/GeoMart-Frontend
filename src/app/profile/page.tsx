3bce11f12f4931b0a0e831fdfd83cb21150d4d21
nitin <nitinnannapaneni@gmail.com>
2025-04-13 02:25:47 +0100

    default profile page added so we can further improve it to store and display users details.

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header';
+
+export default function Profile() {
+  return (
+    <>
+      <Header />
+      <main className="bg-white min-h-screen flex items-center justify-center">
+        <h2 className="text-2xl">Profile Content Goes Here</h2>
+      </main>
+    </>
+  );
+}
